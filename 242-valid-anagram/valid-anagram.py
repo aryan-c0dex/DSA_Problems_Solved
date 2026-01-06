55b7804063dcb5250f5b75df2aeda346240511b8
Aryan <96364057+aryan-c0dex@users.noreply.github.com>
2026-01-07 00:22:04 +0530

    Time: 3 ms (98.58%) | Memory: 19.4 MB (8.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,25 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        if (len(s) != len(t)):
+        if len(s) != len(t):
             return False
-
-        freq1 = {}
-        freq2 = {}
-        for alpha in s:
-            if alpha in freq1:
-                freq1[alpha]+=1
-            else:
-                freq1[alpha] = 1
-
-        for alpha in t:
-            if alpha in freq2:
-                freq2[alpha]+=1
-            else:
-                freq2[alpha] = 1
-            
-        if freq1 == freq2:
-            return True
-        
-        else:
-            return False
+        for i in set(s):
+            if s.count(i) != t.count(i):
+                return False
+        return True
